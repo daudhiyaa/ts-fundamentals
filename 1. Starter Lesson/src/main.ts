0c426d4fa75dad8e5e73bdc39f3a62afc14aec03
Daud Dhiya <daud.dhiya31@gmail.com>
2024-03-02 17:21:00 +0700

    feat: lesson 1 done

---
+++ 1. Starter Lesson/src/main.ts
@@ -0,0 +1,13 @@
+let username = "ur name";
+console.log(username);
+
+let a: number = 12;
+let b: string = "6";
+let c: number = 2;
+
+console.log(a / b);
+
+console.log(c * b);
+
+// kalau gamau ts nya dicompile pas di ts nya ada error
+// go to -> tsconfig.json, "noEmitOnError": true
