88ece6fe8cf70ab97098d0ca6f371d63753f2f24
Daud Dhiya <daud.dhiya31@gmail.com>
2024-03-06 17:48:04 +0700

    refactor: order of files

---
+++ build/js/1. Starter Lesson.js
@@ -0,0 +1,10 @@
+"use strict";
+let username = "ur name";
+console.log(username);
+let a = 12;
+let b = "6";
+let c = 2;
+console.log(a / b);
+console.log(c * b);
+// kalau gamau ts nya dicompile pas di ts nya ada error
+// go to -> tsconfig.json, "noEmitOnError": true
